ebd8e370fcb36defeada3cec9b310e564e92c5a8
Keith Brewster <kbrewster@labx.com>
2018-11-01 23:08:48 -0400

    Added sentiment package. Analyze tweets

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,15 +1,25 @@
 const router = require('express').Router()
 const Twitter = require('../../lib/twitter')
+const utilities = require('./../../lib/utilities')
+const Sentiment = require('sentiment')
 
 const client = new Twitter({
     consumerKey: process.env.REACT_APP_TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.REACT_APP_TWITTER_CONSUMER_SECRET,
     bearerToken: `Bearer ${process.env.REACT_APP_TWITTER_BEARER_TOKEN}`
 })
+const sentiment = new Sentiment()
 
 router.get('/search', async (req, res, next) => {
-    const { search = "hello" } = req.params
-    const tweets = await client.search(search)
+    const { search = "trump" } = req.params
+    let tweets = await client.search(search)
+
+    tweets = tweets.statuses.map(utilities.cleanTweet)
+    
+    for (tweet of tweets) {
+        let score = sentiment.analyze(tweet)
+        console.log(score)
+    }
 
     res.send(tweets)
 })
