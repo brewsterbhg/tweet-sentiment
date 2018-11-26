03010a58199eaec07f1ffea873abf15125028dec
Keith Brewster <kbrewster@labx.com>
2018-11-25 21:30:47 -0500

    Tweak server code and update scripts again

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(session({
 app.use(compression())
 app.use(express.static(path.join(__dirname, '../build')))
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'))
 })
 
