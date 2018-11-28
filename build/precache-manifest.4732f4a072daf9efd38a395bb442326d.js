846d69e045df6dc3a49b5272f5cdf5c372a6d6c5
Keith Brewster <keithbrewster@MacBook-Pro-4.local>
2018-11-28 14:43:18 -0500

    Updated gitignore

---
+++ build/precache-manifest.4732f4a072daf9efd38a395bb442326d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5ac3a798d53e73bc6e3b7fc5e005a39",
+    "url": "/static/media/twitter.f5ac3a79.svg"
+  },
+  {
+    "revision": "a1bce4c7d878ef7d639b768b6dd82504",
+    "url": "/static/media/search.a1bce4c7.svg"
+  },
+  {
+    "revision": "2baa31d7acbdd0dc094209a556956f28",
+    "url": "/static/media/linkedin.2baa31d7.svg"
+  },
+  {
+    "revision": "dd137e26599db07070fe25286efce63c",
+    "url": "/static/media/github.dd137e26.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ac9f18ef360274560fa",
+    "url": "/static/js/main.3ac9f18e.chunk.js"
+  },
+  {
+    "revision": "6bcf2af28e31e905a7ae",
+    "url": "/static/js/1.6bcf2af2.chunk.js"
+  },
+  {
+    "revision": "3ac9f18ef360274560fa",
+    "url": "/static/css/main.28497500.chunk.css"
+  },
+  {
+    "revision": "130ac02ac6a32757b4b10f2e69414a91",
+    "url": "/index.html"
+  }
+];
