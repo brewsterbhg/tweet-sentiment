7a50f47362f0b37ed3d0b6d48034e2000afd4561
Keith Brewster <keith.brewster22@gmail.com>
2019-02-21 14:01:50 -0500

    Updated everything to use prettier

--- server.js
+++ server.js
@@ -1,76 +1,83 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const compression = require('compression')
-const session = require('express-session')
-const MemoryStore = require('memorystore')(session)
-const Twitter = require('./lib/twitter')
-const utilities = require('./lib/utilities')
-const Moment = require('moment')
-require('dotenv').config()
+const express = require("express");
+const path = require("path");
+const app = express();
+const compression = require("compression");
+const session = require("express-session");
+const MemoryStore = require("memorystore")(session);
+const Twitter = require("./lib/twitter");
+const utilities = require("./lib/utilities");
+const Moment = require("moment");
+require("dotenv").config();
 
 const client = new Twitter({
   bearerToken: `Bearer ${process.env.REACT_APP_TWITTER_BEARER_TOKEN}`
-})
+});
 
-app.use(session({
-  store: new MemoryStore({
-    checkPeriod: 86400000
-  }),
-  secret: process.env.REACT_APP_SESSION_SECRET,
-  saveUninitialized: false,
-  resave: false,
-  cookie: { secure: false }
-}))
+app.use(
+  session({
+    store: new MemoryStore({
+      checkPeriod: 86400000
+    }),
+    secret: process.env.REACT_APP_SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: { secure: false }
+  })
+);
 
 function requireHTTPS(req, res, next) {
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-    return res.redirect('https://' + req.get('host') + req.url)
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
   }
   next();
 }
 
-app.use(compression())
+app.use(compression());
 app.use(requireHTTPS);
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/api/search', async (req, res, next) => {
-  const { value } = req.query
+app.get("/api/search", async (req, res) => {
+  const { value } = req.query;
 
   try {
-    const tweets = await client.search(value)
-    const tweetObj = utilities.structureTweetData(tweets)
+    const tweets = await client.search(value);
+    const tweetObj = utilities.structureTweetData(tweets);
 
-    res.send(tweetObj)
+    res.send(tweetObj);
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send(err);
   }
-})
+});
 
-app.get('/api/trending', async (req, res, next) => {
+app.get("/api/trending", async (req, res) => {
   let response;
-  let date = new Moment()
+  let date = new Moment();
 
   try {
     if (
       !req.session.trending ||
-      (req.session.trending &&
-      Moment(req.session.expires).diff(date) <= 0)
+      (req.session.trending && Moment(req.session.expires).diff(date) <= 0)
     ) {
-        response = await client.trending()
-        req.session.trending = response
-        req.session.expires = date.add(5, 'minutes')
+      response = await client.trending();
+      req.session.trending = response;
+      req.session.expires = date.add(5, "minutes");
     } else {
-        response = req.session.trending
+      response = req.session.trending;
     }
-    res.send(response)
+    res.send(response);
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send(err);
   }
-})
+});
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-app.listen(process.env.PORT || 8080, () => console.log(`App listening on port ${process.env.PORT || 8080}`))
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`App listening on port ${process.env.PORT || 8080}`)
+);
