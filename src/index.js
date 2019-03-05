eb6bc3897342c29c98ccdddc001b43cda3b46221
Keith Brewster <keith.brewster22@gmail.com>
2019-03-04 21:36:40 -0500

    Fix for wrong component mounting

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
-import RenderMap from "./components/RenderMap";
 
-ReactDOM.render(<RenderMap />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
